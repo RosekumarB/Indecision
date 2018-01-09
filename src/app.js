60982b825203dd1e3e7f9b5ac5ce1a98ef8f450a
SagarStorm <sagarstorm@gmail.com>
2018-01-09 17:15:39 +0530

    few changes

--- src/app.js
+++ src/app.js
@@ -5,4 +5,7 @@ import Indecision from './components/Indecision.js';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+
+//render the Indecision Component
 ReactDOM.render(<Indecision />, document.getElementById('app'));
